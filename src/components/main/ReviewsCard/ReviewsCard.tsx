f4d5bbd426ef435b501bc950ad51618a5372acc7
FlawlessFellow <flawlesselish@gmail.com>
2024-05-02 16:20:03 +0200

    Created the component ReviewsCard

---
+++ src/components/main/ReviewsCard/ReviewsCard.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './style.css';
+
+interface ReviewsCardProps {
+    classNames: string;
+    text: string;
+    img: string;
+    clientName: string;
+    clientInfoText: string;
+}
+
+const ReviewsCard: React.FC<ReviewsCardProps> = ({ classNames, text, img, clientName, clientInfoText }) => {
+    return (
+        <div>
+            <div className={classNames}>
+                <p className="client-review-text">{text}</p>
+                <div className="client-info-wrapper">
+                    <img src={img} className="client-personal-img" alt="Reviews personal photo" />
+                    <div className="client-info">
+                        <p className="client-name">{clientName}</p>
+                        <p className="client-info">{clientInfoText}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ReviewsCard;
