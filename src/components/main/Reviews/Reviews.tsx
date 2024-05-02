b1d9d06296e3d3f15ec46c0b7f1827b067b5a0cb
FlawlessFellow <flawlesselish@gmail.com>
2024-05-02 14:58:26 +0200

    Added a subtitle to the Reviews section

--- src/components/main/Reviews/Reviews.tsx
+++ src/components/main/Reviews/Reviews.tsx
@@ -4,7 +4,11 @@ import './style.css';
 const Reviews = () => {
     return (
         <div>
-            <section className="reviews"></section>
+            <section className="reviews">
+                <p className="reviews-subtitle">
+                    Відгуки <div className="reviews__subtitle-last-word">клієнтів</div>
+                </p>
+            </section>
         </div>
     );
 };
